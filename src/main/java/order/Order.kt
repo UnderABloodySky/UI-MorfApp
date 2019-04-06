b7c70016913183c143919d99dc6abc72305d87c3
UnderABloodySky <valenzuelahoracioe@gmail.com>
2019-04-06 17:51:10 -0300

    Se agrega import de Date y paquetes con empty class

--- src/main/java/order/Order.kt
+++ src/main/java/order/Order.kt
@@ -3,9 +3,13 @@ package order
 import statesOrder.StateOrder
 import statesOrder.StateOrder.*
 import paymentMethod.*
+import java.util.*
+import user.User
+import restaurant.Restaurant
+import Menu
 
 data class Order(private val code : Int, private val user : User,
-                 private val restaurant : Restaurant, private var payment : PaymentMethod
+                 private val restaurant : Restaurant, private var payment : PaymentMethod,
                  private val menus : MutableList<Menu>){
 
     private var state : StateOrder = PENDING
