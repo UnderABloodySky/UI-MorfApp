3704f2fd3d1d0e0c5201727951e42efd64e7411a
UnderABloodySky <valenzuelahoracioe@gmail.com>
2019-04-09 00:14:25 -0300

    Se agregan tests de la clase FixedDiscount

--- test/discount/TestFixedDiscount.kt
+++ test/discount/TestFixedDiscount.kt
@@ -5,7 +5,9 @@ import org.junit.Test
 
 private class TestFixedDiscount {
 
-    private var fixedDiscountGood : FixedDiscount = FixedDiscount("ejemplo", 100.0)
+    private var fixedDiscountGood100 : FixedDiscount = FixedDiscount("ejemplo", 100.0)
+    private var fixedDiscountGood148 : FixedDiscount = FixedDiscount("ejemplo", 14800.0)
+    private var fixedDiscountGood1000 : FixedDiscount = FixedDiscount("ejemplo", 100.0)
     private var fixedDiscountFool : FixedDiscount = FixedDiscount("ejemplo", 0.0)
     private var n : Double = 1000 as Double
 
@@ -15,8 +17,20 @@ private class TestFixedDiscount {
     }
 
     @Test
-    fun  nineHundredisTheResultOffixedDiscountGoodProcessDiscount(){
-        Assert.assertEquals(900.0, fixedDiscountGood.processDiscount(n))
+    fun  nineHundredisTheResultOffixedDiscountGood100ProcessDiscount(){
+        Assert.assertEquals(900.0, fixedDiscountGood100.processDiscount(n))
+    }
+
+
+    @Test
+    fun  theResultOffixedDiscountGood148ProcessDiscountIs(){
+        Assert.assertEquals(852.0, fixedDiscountGood148.processDiscount(n))
+    }
+
+
+    @Test
+    fun  theResultOffixedDiscountGood1000ProcessDiscountIsZero(){
+        Assert.assertEquals(0.0, fixedDiscountGood1000.processDiscount(n))
     }
 
 }
