c99fae67f9d4006371082bf216401d12d1e4f829
PaisMariano <mariano_a_p@hotmail.com>
2019-04-11 00:18:32 -0300

    detalle de fixedDiscount

--- src/main/java/discount/FixedDiscount.kt
+++ src/main/java/discount/FixedDiscount.kt
@@ -5,9 +5,8 @@ import exception.IncorrectDiscountValueException
 class FixedDiscount(name: String, value : Double ): Discount(name, value) {
 
     override fun discount(price: Double): Double {
-        var localValue = price - this.value;
-        if ((localValue) <= 0)
+        if ((price - this.value) <= 0)
             IncorrectDiscountValueException("The discount can't make the price lesser than 0"); //Chequear como mostrarlo en la vista
-        return localValue;
+        return value;
     }
 }
