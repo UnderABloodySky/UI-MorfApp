6fdb63473049b9d1b3ee91bb64d1c6f19017b857
PaisMariano <mariano_a_p@hotmail.com>
2019-04-21 11:46:26 -0300

    Busqueda de menu por string con tupla

--- src/main/java/restaurant/Restaurant.kt
+++ src/main/java/restaurant/Restaurant.kt
@@ -4,16 +4,18 @@ import user.*
 import paymentMethod.*
 import productAndMenu.*
 import scala.Tuple2
+import searcher.*
 import java.beans.beancontext.BeanContextServiceAvailableEvent
 
 class Restaurant(var code:Int, var name: String, var description: String,
                  var direcction:String, var geoLocation:Geo ) {
     var availablePaymentMethods: MutableCollection<PaymentMethod> = mutableListOf<PaymentMethod>()
-    var products: MutableSet<Product> = mutableSetOf<Product>()
-    var menus: MutableSet<Menu> = mutableSetOf<Menu>();
-    var supervisor:Supervisor? = null
+    var products: MutableMap<Int, Product> = mutableMapOf<Int, Product>()
+    var menus: MutableMap<Int, Menu> = mutableMapOf<Int, Menu>();
+    var supervisor: Supervisor? = null
+    var searcher: Searcher<Menu> = Searcher();
 
-    fun menus():MutableSet<Menu>{
+    fun menus():MutableMap<Int, Menu>{
         return menus;
     }
 
@@ -26,15 +28,15 @@ class Restaurant(var code:Int, var name: String, var description: String,
     }
 
     fun addProductToStock(newProduct:Product){
-        products.add(newProduct);
+        products.put(newProduct.code, newProduct);
     }
 
     fun removeProductFromStock(productToRemove: Product){
-        products.remove(productToRemove);
+        products.remove(productToRemove.code);
     }
 
     fun productIsContainedInTheStock(product: Product):Boolean{
-        return products.contains(product);
+        return products.containsValue(product);
     }
 
     fun addPaymentMethod(newPaymentMethod: PaymentMethod){
@@ -44,20 +46,26 @@ class Restaurant(var code:Int, var name: String, var description: String,
         availablePaymentMethods.remove(payment);
     }
     fun addMenu(newMenu: Menu){
-        menus.add(newMenu);
+        menus.put(newMenu.code, newMenu);
     }
 
     fun menuIsRegistered(menu: Menu):Boolean{
-        return menus.contains(menu);
+        return menus.containsValue(menu);
     }
     //ver esto que onda, ver si es necesario ver si exite antes de borrar o lo hace la fucion remove.
     fun removeMenu(menuToRemove:Menu){
-        menus.remove(menuToRemove);
+        menus.remove(menuToRemove.code);
     }
 
-    fun menusAvailable():List<Menu>{
-       return menus.filter {m -> m.enabled}
+    fun menusAvailable() : Map<Int, Menu>{
+       return menus.filter {m -> m.value.enabled}
     }
 
+    fun findMenuesPair(criteria: CriteriaByString<Menu>): MutableList<Pair<Menu?, Restaurant>>{
+        var pairList = mutableListOf<Pair<Menu?, Restaurant>>();
+        criteria.search(this.menus)
+                .forEach { pairList.add(Pair(it, this)) };
+        return pairList;
+    }
 
 }
