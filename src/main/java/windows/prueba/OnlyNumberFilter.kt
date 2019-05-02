e76ff2fae69d10e9496d4488d9fb62ef17f3b30f
UnderABloodySky <valenzuelahoracioe@gmail.com>
2019-05-02 16:18:39 -0300

    Se agrega OnlyTextBox

--- src/main/java/windows/prueba/OnlyNumberFilter.kt
+++ src/main/java/windows/prueba/OnlyNumberFilter.kt
@@ -4,12 +4,10 @@ import org.uqbar.arena.filters.TextFilter
 import org.uqbar.arena.widgets.TextInputEvent
 import org.uqbar.commons.model.exceptions.UserException
 
-class SearchFilter : TextFilter {
+class OnlyNumberFilter : TextFilter {
 
     override fun accept(event: TextInputEvent): Boolean {
-        // val pattern_onlyAlphanumeric = "[^A-Za-z0-9 ]"
         var result = false
-        //return event.potentialTextResult.matches(Regex("[^A-Za-z0-9 ]"))
         try {
             Integer.parseInt(event.potentialTextResult)
             result = true
