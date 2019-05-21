8dc89e6383373ced2e44bd23222b6ba99f68976b
PaisMariano <mariano_a_p@hotmail.com>
2019-05-21 19:33:37 -0300

    Agrego clases de la API

---
+++ src/main/java/api/menuController.kt
@@ -0,0 +1,6 @@
+package api
+
+data class Product(var id: Int)
+data class Menu(var id: Int)
+
+class menuController{}
---
+++ src/main/java/api/restaurantController.kt
@@ -0,0 +1,6 @@
+package api
+
+data class PaymentMethod(var id: Int)
+data class Restaurant(var id: Int)
+
+class RestaurantController{}
