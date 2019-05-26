0be35b2c6da760be1bf33cf1f26b83f0ae09b914
PaisMariano <mariano_a_p@hotmail.com>
2019-05-26 11:45:39 -0300

    api pega directamente al modelo

--- src/main/java/api/MainMariano.kt
+++ src/main/java/api/MainMariano.kt
@@ -5,9 +5,12 @@ import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
 import org.eclipse.jetty.http.HttpStatus.*
 import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import discount.FixedDiscount
 import discount.NoDiscount
 import geoclaseui.Geo
+import paymentMethod.Cash
 import productAndMenu.Category
+import productAndMenu.Menu
 import productAndMenu.Product
 
 fun main() {
@@ -24,31 +27,67 @@ fun main() {
 
     app.get("/") { ctx -> ctx.json(mapOf("message" to "Hello World")) }
 
-    var unaHamburguesaSalvaje = Product(0,"Hamburguesa", "Al vapor", 100.00, Category.NINGUNO)
+    //var unaHamburguesaSalvaje = Product(0,"Hamburguesa", "Al vapor", 100.00, Category.NINGUNO)
 
     val geo = Geo(123.0, 123.0, "123")
     val availablePM = mutableListOf<PaymentMethod>()
 
-    val restaurantController = RestaurantController()
+    var morfap = MorfApp
+    var laConga = morfap.createRestaurant("La Conga",
+            "Cocina Peruana",
+            "Calle Falsa 123",
+            Geo(1.5, 1.5),
+            mutableListOf(Cash()))
+    var clubMili = morfap.createRestaurant("El club de la milanesa",
+            "Mili de plastico",
+            "Rigoletto 245",
+            Geo(1.7, 1.8),
+            mutableListOf(Cash()))
+    morfap.createSupervisor(clubMili, "pepe", "Pepe","1234")
+    morfap.createSupervisor(laConga, "...", "Beto","...")
 
-    val tempRest = Restaurant(1, "Pipox", "Peru", "falsa 123", geo, availablePM)
+    var unaHamburguesaSalvaje : Product = laConga.createProduct("Hamburguesa", "Al vapor", 100.00, Category.NINGUNO)
+    var unaCocaSalvaje : Product = laConga.createProduct("Coca Cola", "Azucar 200%", 60.00, Category.BEBIDA)
+    var menu1: Menu = laConga.createMenu("Menu1",
+            "Coca + Hambur",  mutableListOf<Product>(), laConga, NoDiscount(), false)
+    menu1.addProductToMenu(unaHamburguesaSalvaje)
+    menu1.addProductToMenu(unaCocaSalvaje)
 
-    restaurantController.addRestaurant(tempRest)
+    var porcionDePapas : Product = laConga.createProduct("Papas fritas", "papas", 80.00, Category.ADICIONAL)
+    var rabas : Product = laConga.createProduct("Rabas", "rabas fritas", 160.00, Category.ENTRADA)
 
-    restaurantController.menus.add(Menu(0, "pep", "a", mutableListOf(unaHamburguesaSalvaje), tempRest, NoDiscount(), true))
+    var menu2: Menu = laConga.createMenu("Menu2",
+            "Coca + Hambur",  mutableListOf<Product>(), laConga, FixedDiscount(20.00), true)
 
-    restaurantController.addRestaurant(
-            Restaurant(2, "Pepex", "Peru", "falsa 234", geo, availablePM))
-    restaurantController.addRestaurant(
-            Restaurant(3, "Milanga", "Peru", "falsa 345", geo, availablePM))
+    menu2.addProductToMenu(rabas)
+    menu2.addProductToMenu(porcionDePapas)
+    menu2.addProductToMenu(unaHamburguesaSalvaje)
+
+    val restaurantController = RestaurantController2()
+
+    //val tempRest = Restaurant(1, "Pipox", "Peru", "falsa 123", geo, availablePM)
+
+    //restaurantController.addRestaurant(tempRest)
+
+    //restaurantController.menus.add(Menu(0, "pep", "a", mutableListOf(unaHamburguesaSalvaje), tempRest, NoDiscount(), true))
+
+    //restaurantController.addRestaurant(
+    //        Restaurant(2, "Pepex", "Peru", "falsa 234", geo, availablePM))
+    //restaurantController.addRestaurant(
+    //        Restaurant(3, "Milanga", "Peru", "falsa 345", geo, availablePM))
 
     // CRUD de Lugares
     // Sintaxis alternativa, mucho más concisa
     // Donde el comportamiento se traslada al controller
 
     app.routes {
+        path("restaurants"){
+            path(":code") {
+                get(restaurantController::getAllMenus)
+            }
+        }
         path("search"){
-                get(restaurantController::getRestaurant)
+            get(restaurantController::getRestaurantsAndMenusByCriteria)
         }
 
     }
--- src/main/java/user/Supervisor.kt
+++ src/main/java/user/Supervisor.kt
@@ -2,10 +2,15 @@ package user
 import restaurant.*
 import productAndMenu.*
 import applicationModel.*
+import com.fasterxml.jackson.annotation.JsonIgnore
 import paymentMethod.PaymentMethod
 
-class Supervisor( code : Int, name : String, id : String , val restaurant: Restaurant, password: String, applicationModel: MorfApp):
-        User (code, name, id, password, applicationModel) {
+class Supervisor(code: Int,
+                 name: String,
+                 id : String,
+                 @JsonIgnore val restaurant: Restaurant,
+                 password: String,
+                 applicationModel: MorfApp): User (code, name, id, password, applicationModel) {
 
     fun addProductToRestaurantStock(newProduct: Product) = restaurant.addProductToStock(newProduct)
 
