509913ea7c77585bc08b8900c837ae70181c6c8b
UnderABloodySky <valenzuelahoracioe@gmail.com>
2019-06-06 11:28:53 -0300

    Se arreglan tests por los nuevos parametros agregados en los paymentMethods

--- test/searcher/TestSearcher.kt
+++ test/searcher/TestSearcher.kt
@@ -11,6 +11,7 @@ import productAndMenu.Menu
 import productAndMenu.Product
 import applicationModel.MorfApp
 import restaurant.Restaurant
+import java.util.*
 
 class TestSearcher {
 
@@ -18,7 +19,7 @@ class TestSearcher {
     private val geoLocation = Geo(2.0, 1.0)
     private val applicationModel = MorfApp
     private val cash = Cash()
-    private val debit = Debit()
+    private val debit = Debit("", 123, 1, Date())
     private val paymentMethods = mutableListOf(cash, debit)
     private val restaurant0: Restaurant = applicationModel.createRestaurant("Bar 144234", "LLenadero magico de tripas", "Por Quilmes Oeste", geoLocation, paymentMethods)
     private val restaurant1: Restaurant = applicationModel.createRestaurant("Le Guerrin", "The best pizza of Bs. As.", "Corrientes 4321", geoLocation, paymentMethods)
