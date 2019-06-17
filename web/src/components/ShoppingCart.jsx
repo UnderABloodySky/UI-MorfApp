421ec09b54687cddd21d2e0c9112e2278b5beadb
PaisMariano <mariano_a_p@hotmail.com>
2019-06-17 00:18:43 -0300

    shopping cart con agregados funcionales

--- web/src/components/ShoppingCart.jsx
+++ web/src/components/ShoppingCart.jsx
@@ -18,8 +18,7 @@ export default class ShoppingCart extends React.Component {
         };
         this.state.id = this.props.id;
         this.state.code = this.props.code;
-        //this.state.availableMenus = this.props.availableMenus;
-        //this.state.selectedMenus = this.props.selectedMenus;
+        //this.state.availableMenus = this.props.availableMenus; // ESPERO A QUE HORA ME PASE LOS PROPS DESDE EL BOTON DEL PEDIDO
     }
 
     toPayment = () => {
@@ -34,56 +33,96 @@ export default class ShoppingCart extends React.Component {
           toOrders: true,
           toPayment: false
         })
-    };
+    }; 
 
-    getTotalPrice() {
-        var ammount = 0;
-        this.state.availableMenus.productsOfMenu.forEach(element => {ammount += element.price});
-        return ammount;
+    addMenuToOrder(givenMenu){
+        var found = false;
+        for(var i = 0; i < this.state.selectedMenus.length; i++){
+            if (this.state.selectedMenus[i].menu.code === givenMenu.code){
+                found = true;
+                if (found){
+                    this.state.selectedMenus[i].ammount++
+                    this.setState({selectedMenus : this.state.selectedMenus});
+                }
+                break;
+            }
+        }
+        if (!found){
+            var tempObj = { ammount: 1, menu: givenMenu };
+            this.setState({selectedMenus: this.state.selectedMenus.concat([tempObj])});
+        }
     }
+
+    addOne(givenMenu){
+        givenMenu.ammount++;
+        this.setState({selectedMenus : this.state.selectedMenus});
+    }
+    removeOne(givenMenu){
+        if(givenMenu.ammount > 1){
+            givenMenu.ammount--;
+            this.setState({selectedMenus : this.state.selectedMenus});
+        }else{
+            
+        }        
+    }
+    renderSelectedMenus(){
+        return(
+                (menus) =><li key={menus.menu.code}>
+                    <div className="col-md-4" >
+                        <div className="card mt-4">
+                        <div className="card-headercard-title text-center">
+                            Código Menu: {menus.menu.code}
+                        </div>
+                            <div className="card-body">
+                                <p>Menú: {menus.menu.name}</p>
+                                <p>Cantidad: {menus.ammount} Precio unitario: {menus.menu.productsOfMenu.reduce(
+                                                                                function(prev, cur) {return prev + cur.price; }
+                                                                                , 0)}$                                                                    
+                                Precio: {menus.menu.productsOfMenu.reduce(
+                                            function(prev, cur) {return prev + cur.price; }
+                                            , 0) * menus.ammount}$
+                                <button className="btn btn-danger" onClick={() => this.removeOne(menus)}>-</button>
+                                <button className="btn btn-danger" onClick={() => this.addOne(menus)}>+</button>
+                                </p>                                                                             
+                                                            
+                                <button className="btn btn-danger" onClick={() => this.addMenuToOrder(menus)}>Quitar</button>
+                            </div>
+                        </div>  
+                    </div>
+                </li>
+        )
+    }
+
+    renderAvailableMenus(){
+        return(
+                (menus) =><li key={menus.code}>
+                    <div className="col-md-4" >
+                        <div className="card mt-4">
+                        <div className="card-headercard-title text-center">
+                            <h3>Código Menu: {menus.code}</h3>
+                        </div>
+                            <div className="card-body">
+                                <p>Menú: {menus.name}</p>
+                                <p>Precio: {menus.productsOfMenu.reduce(
+                                            function(prev, cur) {return prev + cur.price; }
+                                            , 0)}                                                                             
+                                $</p>                                                                
+                                <button className="btn btn-danger" onClick={() => this.addMenuToOrder(menus)}>Agregar</button>
+                            </div>
+                        </div>  
+                    </div>
+                </li>
+        )
+    }    
 
     componentDidMount(){    
         getMenus(0)
-        .then(result => {
-            console.log(result);
+        .then(result => { 
             this.setState({
             availableMenus: result })})       
-    }    
+    }
 
     render(){
-        const mappingSelectedMenus = (menus) =><li key={menus.code}>
-                                                    <div className="col-md-4" >
-                                                        <div className="card mt-4">
-                                                        <div className="card-headercard-title text-center">
-                                                            <h3>Código Menu: {menus.code}</h3>
-                                                        </div>
-                                                            <div className="card-body">
-                                                                <p>Menú: {menus.name}</p>
-                                                                <p>Cantidad: {menus.ammountOfMenus}</p>
-                                                                <p>Precio: {menus.price} $</p>
-                                                                <button className="btn btn-danger">Quitar</button>
-                                                            </div>
-                                                        </div>  
-                                                    </div>
-                                                </li>
-        const mappingAvailableMenus = (menus) =><li key={menus.code}>
-                                                    <div className="col-md-4" >
-                                                        <div className="card mt-4">
-                                                        <div className="card-headercard-title text-center">
-                                                            <h3>Código Menu: {menus.code}</h3>
-                                                        </div>
-                                                            <div className="card-body">
-                                                                <p>Menú: {menus.name}</p>
-                                                                <p>Precio: 
-                                                                        {menus.productsOfMenu.reduce(
-                                                                            function(prev, cur) {return prev + cur.price; }
-                                                                            , 0)}                                                                             
-                                                                $</p>                                                                
-                                                                <button className="btn btn-danger">Agregar</button>
-                                                            </div>
-                                                        </div>  
-                                                    </div>
-                                                </li>
         
         if(this.state.toOrders && !this.state.toPayment){
             return(<Redirect to={{
@@ -96,17 +135,17 @@ export default class ShoppingCart extends React.Component {
                         state: { id: this.state.id, password: this.state.password } }}/>)
         }
         return( <div>
-                    <div className="product-price">Su pedido:</div>
+                    <div>Su pedido: </div>
                         <div>
                             <ul>
-                                {this.state.selectedMenus.map(mappingSelectedMenus)}
+                                {this.state.selectedMenus.map(this.renderSelectedMenus())}
                             </ul>
                         </div>
 
                     <div>Agregue productos del listado:</div>
                         <div>
                             <ul>
-                                {this.state.availableMenus.map(mappingAvailableMenus)}
+                                {this.state.availableMenus.map(this.renderAvailableMenus())}
                             </ul>
                         </div>
                     <button className="btn btn-danger" onClick={this.backToOrders}>Volver a Ordenes</button>
