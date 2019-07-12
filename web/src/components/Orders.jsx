f0c5b186d8a078df9593c167b5b24ec805f20cf0
jlajcha <juliana.lajcha@gmail.com>
2019-07-11 23:04:35 -0300

    cambios en la vista de ordenes

--- web/src/components/Orders.jsx
+++ web/src/components/Orders.jsx
@@ -4,11 +4,9 @@ import { Redirect } from 'react-router-dom'
 import { getPendingOrdersFrom } from '../api/api'
 import { getHistoricOrdersFrom } from '../api/api'
 import { ratePendingOrder } from '../api/api'
-import StarComponent from './StarComponent'
-import Popup from "reactjs-popup";
+import StarRatingComponent from 'react-star-rating-component'
 import './css/Orders.css';
 import './css/Body.css'
-//import './css/ShoppingCart.css';
 
 export default class Orders extends React.Component {
     constructor(props) {
@@ -17,32 +15,35 @@ export default class Orders extends React.Component {
           id: '',
           pendingOrders: [],  
           historicOrders: [],
-          rating: 1
+          rating: 1,
+          toComponent:false
         };
+        
+        this.rateOrder = this.rateOrder.bind(this);
         if (this.props.location.state !== undefined){
           this.state.id = this.props.location.state.id;  //Chequeo que haya venido una props
         }
       }
-
     
-  onStarClick(nextValue, prevValue, name) {
-    this.setState({rating: nextValue});
-
-  }
-    priceOfOrder(order){      
+      onStarClick(nextValue, prevValue, name) {
+        this.setState({rating: nextValue});
+        
+      }
+   
+    priceOfOrder(order){
       var total = 0;
       order.menus.map(element => { total = total + element.price });
       return total;  
     }
-//ver como hacer que tome en el body el rate y el codigo de la orden.
-// cuando se da aceptar tiene que pasar de pending a historic order .. ver 
-   // rateOrder(order,rating){
-     //   order.setState.rating()
-       // ratePendingOrder(this.state.id,order.id,)
 
+rateOrder(nextValue,codeOrder) {
 
-    //}
-
+     // this.setState({rating: nextValue});
+      ratePendingOrder({ code_order:codeOrder , rating: nextValue})
+        .then(() => this.setState({ toComponent: true }))
+        .catch(() => this.setState({}));
+    }
+  
     componentDidMount(){
        getPendingOrdersFrom(this.state.id)
         .then(result => { 
@@ -53,31 +54,42 @@ export default class Orders extends React.Component {
     }
 
     render() {
-      const mappingOrderCode = (order) => (<li key={order.code_order_complete}>
-                                          <div className="container1" >
-                                              
-                                                <div className="card-headercard-title text-center">
-                                                  <h3>Código Orden: {order.code_order_complete}</h3>
-                                                </div>
-                                                  <div className="card-body">
-                                                    <p><h5>Restaurant: {order.restaurantName}</h5></p>
-                                                    <p><mark>{order.menus.map(itMenus => (<li key={itMenus.code}>
-                                                                                                            <p><h6>Menú: {itMenus.name}</h6></p>
-                                                                                                            <p>Cantidad: {itMenus.ammountOfMens}</p>
-                                                                                                            <p>Precio: {itMenus.price} $</p>
+      
+      if (this.state.toComponent){
+                  return <Redirect to={{
+                    pathname: '/orders',
+                    state: { id: this.state.id, password: this.state.password } }}/>
+        }
 
-                                                                                         </li>))}
-                                                      <br></br>
-                                                      <p><h4>Precio Total: {this.priceOfOrder(order)} $</h4></p>
-                                                      </mark>
-                                                    </p>
-                                                    <StarComponent />
-                                                    {<button className ="btn btn-success" > Aceptar </button> }
-                                                        <button className="btn btn-danger">Cancelar</button>
-                                                       
-                                                   </div>
-                                                  </div>  
-                                                
+      const mappingOrderCode = (order) => (<li key={order.code_order_complete}>
+                <div className="container1" >
+                    <div className="card-headercard-title text-center">
+                        <h3>Código Orden: {order.code_order_complete}</h3>
+                      </div>
+                        <div className="card-body">
+                          <p><h5>Restaurant: {order.restaurantName}</h5></p>
+                          <p><mark>{order.menus.map(itMenus => (<li key={itMenus.code}>
+                                                                  <p><h6>Menú: {itMenus.name}</h6></p>
+                                                                  <p>Cantidad: {itMenus.ammountOfMens}</p>
+                                                                  <p>Precio: {itMenus.price} $</p>
+                                                              </li>))}
+                          <br></br>
+                          <p><h4>Precio Total: {this.priceOfOrder(order)} $</h4></p>
+                          </mark>
+                          </p>
+                            <StarRatingComponent
+                                  name = "rate1"
+                                  starCount = {5}
+                                  value = {this.state.rating}
+                                 onStarClick = {this.onStarClick.bind(this)}
+                                />
+                            {<button className ="btn btn-success" 
+                                      onClick={this.rateOrder(this.state.rating,order.code_order_complete)}>
+                                         Aceptar </button> }
+                            <button className="btn btn-danger">Cancelar</button>
+                      </div>
+                  </div>  
+                      
                                           </li>)
       
       if (this.state.id === ''){
@@ -90,7 +102,6 @@ export default class Orders extends React.Component {
                 <br></br>
                 <div><h3>Ordenes Pendientes</h3></div>
                   <div>
-                  
                       <ul>
                          <div className="grid-container3">
                       
